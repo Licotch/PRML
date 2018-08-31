3fa517b5e4c41c9baaad54996ea618dd52864977
Licotch <licotch@licot.ch>
2018-09-01 07:50:46 +0900

    remove cl-fad

--- chapter1/src/fig1-02.lisp
+++ chapter1/src/fig1-02.lisp
@@ -1,6 +1,5 @@
 (ql:quickload '(vgplot
-                alexandria
-                cl-fad))
+                alexandria))
 
 (let* ((x1 (vgplot:range 0 101/100 1/100))
        (y1 (map 'vector #'(lambda (x) (sin (* 2 pi x))) x1))
