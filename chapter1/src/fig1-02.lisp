3fb544860c5730339dd94090e0770b8d5a3ecd5a
Licotch <licotch@licot.ch>
2018-09-01 13:25:40 +0900

    modify for lake

--- chapter1/src/fig1-02.lisp
+++ chapter1/src/fig1-02.lisp
@@ -1,9 +1,14 @@
-(ql:quickload '(vgplot
-                alexandria)
+(ql:quickload '(:vgplot
+                :alexandria)
               :silent t)
 
-(defun main (&rest args)
-  (declare (ignorable args))
+(in-package :cl-user)
+(defpackage :prml.chapter1
+  (:use :cl)
+  (:export :fig1-02))
+(in-package :prml.chapter1)
+
+(defun fig1-2 ()
   (let* ((x1 (vgplot:range 0 101/100 1/100))
          (y1 (map 'vector #'(lambda (x) (sin (* 2 pi x))) x1))
          (x2 (vgplot:range 0 10/9 1/9))
@@ -18,5 +23,8 @@
     (vgplot:format-plot t "set ytics 1")
     (vgplot:xlabel "x")
     (vgplot:ylabel "t")
-    (vgplot:print-plot #P"fig1-02.png")
-    (sleep 5)))
+    (vgplot:print-plot #P"chapter1/img/fig1-02.png")
+    (sleep 3)
+    (vgplot:close-all-plots)))
+
+(fig1-2)
