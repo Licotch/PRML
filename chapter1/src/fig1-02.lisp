05165fe0c4ec303b2b2d4f95fb859d68a90c81bb
Licotch <licotch@licot.ch>
2018-09-02 13:40:48 +0900

    print-plot is not useful

--- chapter1/src/fig1-02.lisp
+++ chapter1/src/fig1-02.lisp
@@ -12,6 +12,9 @@
   (let* ((x1 (vgplot:range 0 101/100 1/100))
          (y1 (map 'vector #'(lambda (x) (sin (* 2 pi x))) x1))
          (data (vgplot:load-data-file "data.csv")))
+    (vgplot:format-plot t "set terminal pngcairo")
+    (vgplot:format-plot t "set output 'chapter1/img/fig1-02.png'")
+
     (vgplot:plot x1 y1 ""
                  (first data) (second data) "og;")
     (vgplot:axis '(-0.1 1.1 -1.5 1.5))
@@ -20,8 +23,7 @@
     (vgplot:format-plot t "set ytics 1")
     (vgplot:xlabel "x")
     (vgplot:ylabel "t")
-    (vgplot:print-plot #P"chapter1/img/fig1-02.png")
-    (sleep 3)
-    (vgplot:close-all-plots)))
+
+    (vgplot:format-plot t "unset output")))
 
 (fig1-2)
