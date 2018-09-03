d7938a4b31a01dc06d6c283ed47b0388f40703a3
Licotch <licotch@licot.ch>
2018-09-03 15:25:37 +0900

    add the vgplot:new-plot function in fig1-? function

--- src/chapter1/fig1-02.lisp
+++ src/chapter1/fig1-02.lisp
@@ -7,6 +7,8 @@
   (let* ((x1 (vgplot:range 0 101/100 1/100))
          (y1 (map 'vector #'(lambda (x) (sin (* 2 pi x))) x1))
          (data (vgplot:load-data-file (asdf:system-relative-pathname :prml "data/data.csv"))))
+    (vgplot:new-plot)
+
     (vgplot:plot x1 y1 "g;"
                  (first data) (second data) "ob;")
     (vgplot:axis '(-0.1 1.1 -1.5 1.5))
