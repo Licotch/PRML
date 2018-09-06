eb00d10b167923590ac3253afab08c64985694d7
Licotch <licotch@licot.ch>
2018-09-06 10:12:28 +0900

    changed the variable name to be easy to understand

--- src/chapter1/fig1-02.lisp
+++ src/chapter1/fig1-02.lisp
@@ -1,11 +1,13 @@
 (in-package :prml.chapter1)
 
 (defun fig1-2 ()
-  (let* ((x1 (vgplot:range 0 101/100 1/100))
-         (y1 (map 'vector #'(lambda (x) (sin (* 2 pi x))) x1))
-         (data (vgplot:load-data-file (asdf:system-relative-pathname :prml "data/data.csv"))))
-    (vgplot:plot x1 y1 "g;"
-                 (first data) (second data) "ob;")
+  (let* ((sin-x (vgplot:range 0 101/100 1/100))
+         (sin-y (map 'vector #'(lambda (x) (sin (* 2 pi x))) sin-x))
+         (traingin-set (vgplot:load-data-file (asdf:system-relative-pathname :prml "data/data.csv")))
+         (observed-x (first traingin-set))
+         (observed-y (second traingin-set)))
+    (vgplot:plot sin-x sin-y "g;"
+                 observed-x observed-y "ob;")
     (vgplot:axis '(-0.1 1.1 -1.5 1.5))
     (vgplot:grid nil)
     (vgplot:format-plot nil "set xtics 1")
